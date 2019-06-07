18c649d47049d31f34bd7ae1b056d37277f3df0a
Hassan Hamdy <15hhamdy@gmail.com>
2019-06-07 06:05:57 +0200

    creating cup page and its card class

---
+++ lib/UI/cup.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CupOfNation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: new Text("Africa Cup of Nation"),
+        centerTitle: true,
+      ),
+    );
+  }
+}
+
+class MatchCard extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return null;
+  }
+}
